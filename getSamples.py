8d15a3e902ba0d93c89ba9bac95cb362e313f998
timjzee <timjzee@gmail.com>
2017-01-08 19:07:50 +0100

    restructured directories

--- getSamples.py
+++ getSamples.py
@@ -1,15 +1,16 @@
 import os
 import re
+import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 
-# SAMPLE_SIZES = [200, 600, 1000, 1400, 1800, 2200]
-# MAX_SAMPLE_SIZE = max(SAMPLE_SIZES)
-MAX_SAMPLE_SIZE = 2200
-SAMPLE_SIZE = 200
+SAMPLE_SIZES = [200, 600, 1000, 1400, 1800, 2200]
+MAX_SAMPLE_SIZE = max(SAMPLE_SIZES)
+# MAX_SAMPLE_SIZE = 2200
+# SAMPLE_SIZE = 200
 
 
 def getFileNames():
-    input_path = "/Users/tim/GitHub/frankenstein/author_known/"
+    input_path = "/Users/tim/GitHub/frankenstein/clean_texts/author_known/"
     temp_list = os.listdir(input_path)
     name_list = [i for i in temp_list if i[-4:] == ".txt"]
     return name_list, input_path
@@ -31,6 +32,8 @@ def getSamples(token_list, whole_text):
     list_of_samples = []
     sample_start_index = 0
     for i in range(SAMPLE_SIZE, sampling_max + 1, SAMPLE_SIZE):
+        if i % (10 * SAMPLE_SIZE) == 0:
+            print("Working on sample {}".format(int(i / SAMPLE_SIZE)))
         lower_boundary = i - SAMPLE_SIZE
         token_sublist = token_list[lower_boundary:i]
         sample_end_index = re.search(r'[\W]*' + r'[\W]+'.join(token_sublist), whole_text, re.I).end() + 1
@@ -46,20 +49,31 @@ def processTexts():
     all_samples = []
     category_nms = []
     category_lbls = []
-    category_index = 0
     for name in file_names:
-        category_nms.append(name[:len(file_names)])
+        if name[:3] not in category_nms:
+            category_nms.append(name[:3])
+        category_index = len(category_nms) - 1
         f = open(file_path + name, "r", encoding="utf-8")
         text = f.read()
         f.close
         word_list = getInfo(text)
-        print(name + ": " + str(len(word_list)) + " words")
+        print("Start sampling {} ({} words) with a sample size of {}".format(name, len(word_list), SAMPLE_SIZE))
         text_samples = getSamples(word_list, text)
         all_samples.extend(text_samples)
         num_samples = len(text_samples)
         category_lbls.extend([category_index for i in range(num_samples)])
-        category_index += 1
     return all_samples, category_lbls, category_nms
 
 
-samples, category_labels, category_names = processTexts()
+def saveSamples():
+    """Save samples, category_labels and category_names to a pickle of a list."""
+    samples_path = "/Users/tim/GitHub/frankenstein/sampled_texts/"
+    samples_structure = [samples, category_labels, category_names]
+    g = open(samples_path + "samples_" + str(SAMPLE_SIZE) + ".pck", "wb")
+    pickle.dump(samples_structure, g)
+    g.close()
+
+
+for SAMPLE_SIZE in SAMPLE_SIZES:
+    samples, category_labels, category_names = processTexts()
+    saveSamples()
